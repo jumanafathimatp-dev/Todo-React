18ea17eda5df5f26ad37bd17ec105fa788df043c
jumanafathimatp-dev <jumanafathimatp@gmail.com>
2026-05-29 11:42:39 +0530

    full api add

--- src/UseTodo.js
+++ src/UseTodo.js
@@ -1,97 +1,97 @@
-import { useState, useEffect}from "react";
+import { useState, useEffect } from "react";
 
-function UseTodo(){
+function UseTodo() {
+  const [todos, setTodos] = useState([])
+   
 
-     const [todos, setTodos] = useState(() => {
-
-    const storedTodos =
-      localStorage.getItem("todos");
-
-    return storedTodos
-      ? JSON.parse(storedTodos)
-      : [];
-
-  });
-
-   useEffect(() => {
-
-    if (todos.length === 0) {
-
+  useEffect(() => {
+    
       fetch("https://dummyjson.com/todos")
         .then((res) => res.json())
         .then((data) => {
-
           setTodos(data.todos);
-
         });
+    
+  }, []);
 
-    }
-
-  }, [])
- 
-      useEffect(() => {
-    
-        localStorage.setItem(
-          "todos",
-          JSON.stringify(todos)
-        );
-    
-      }, [todos]);
-    
-      
-      function addTodo(text) {
-    
-        if (!text.trim()) return;
-
-    
-        setTodos([
-          ...todos,
-          {
-            text:text,
-            completed: false,
-          },
-        ]);
-      }
-       
-      function deleteTodo(index) {
-    
-        const newTodos = todos.filter(
-          (todo, i) => i !== index
-        );
-    
-        setTodos(newTodos);
-      }
-    
-     
-      function updateTodo(index, text) {
-    
-        const updatedTodos = [...todos];
-    
-        updatedTodos[index].todo = text;
-    
-        setTodos(updatedTodos);
-      }
-    
-      
-      function toggleComplete(index) {
-    
-        const updatedTodos = [...todos];
-    
-        updatedTodos[index].completed =
-          !updatedTodos[index].completed;
-    
-        setTodos(updatedTodos);
-      }
-
-      return {
-  todos,
-  addTodo,
-  deleteTodo,
-  updateTodo,
-  toggleComplete,
-};
-
-}
   
+
+  function addTodo(text) {
+    if (!text.trim()) return;
+
+    fetch("https://dummyjson.com/todos/add", {
+      method: "POST",
+
+      headers: {
+        "Content-Type": "application/json",
+      },
+
+      body: JSON.stringify({
+        todo: text,
+        completed: false,
+        userId: 5,
+      }),
+    })
+      .then((res) => res.json())
+      .then((newTodo) => {
+        setTodos([...todos, newTodo]);
+      });
+  }
+
+  
+
+  function deleteTodo(index) {
+    
+    fetch(`https://dummyjson.com/todos/1`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then(() => {
+        const newTodos = todos.filter((todo, i) => i !== index);
+
+        setTodos(newTodos);
+      });
+  }
+
+  function updateTodo(index, text) {
+    
+
+    fetch(`https://dummyjson.com/todos/1`, {
+      method: "PUT",
+
+      headers: {
+        "Content-Type": "application/json",
+      },
+
+      body: JSON.stringify({
+        todo: text,
+      }),
+    })
+      .then((res) => res.json())
+      .then((updatedTodo) => {
+        const updatedTodos = [...todos];
+
+        updatedTodos[index].todo = updatedTodo.todo;
+
+        setTodos(updatedTodos);
+      });
+  }
+
+  function toggleComplete(index) {
+    const updatedTodos = [...todos];
+
+    updatedTodos[index].completed = !updatedTodos[index].completed;
+
+    setTodos(updatedTodos);
+  }
+
+  return {
+    todos,
+    addTodo,
+    deleteTodo,
+    updateTodo,
+    toggleComplete,
+  };
+}
 
 export default UseTodo;
